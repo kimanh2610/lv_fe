4b2a7332ded4709d9ee36a8049c8e60d671b82d7
HuynhKimAnh <huynhkimanhh2610@gmail.com>
2022-10-13 21:30:53 +0700

    code editor

--- src/pages/Home/Lesson/ContentLesson/index.jsx
+++ src/pages/Home/Lesson/ContentLesson/index.jsx
@@ -4,7 +4,17 @@ import './index.scss';
 export default function ContentLesson() {
   return (
     <div className="content_lesson_wrapper">
-      ContentLesson
+      <div className="ct_less_nav">
+        <div className="ct_less_nav_content">
+          <div className="leaderboard"></div>
+          <h1>Hướng dẫn HTML</h1>
+          <div className='btn_nextprev'>
+            <a className='btn_prev lc_btn' href="/">&larr; Trang chủ</a>
+            <a className='btn_next lc_btn' href="#">Kế tiếp &rarr;</a>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/pages/Home/Lesson/index.jsx
+++ src/pages/Home/Lesson/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Layout from '../../../layout/client'
 import SidebarLeft from '../SidebarLeft'
 import ListLesson from './ListLesson'
 import './index.scss'
